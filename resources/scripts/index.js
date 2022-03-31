2062e665b8134b9a4cb14cc587a67a9352c34a15
Brendan Payne <payneb2@mail.uc.edu>
2022-03-31 14:07:33 -0400

    eventlisteners are now functional

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,4 +1,6 @@
-function createPentagon(id, size = 100, Xcenter = 150, Ycenter = 150, shiftVal = 0, color = "red") {
+
+
+function createPentagon(id, size = 100, Xcenter = 150, Ycenter = 150, shiftVal = 0, color = "red", letter = "A") {
     // pentagon
     const numberOfSides = 5,
         step  = 2 * Math.PI / numberOfSides, //Precalculate step value
@@ -21,23 +23,62 @@ function createPentagon(id, size = 100, Xcenter = 150, Ycenter = 150, shiftVal =
     pentagon.setAttribute("points", points);
     pentagon.setAttribute("fill", color);
     svg.appendChild(pentagon);
+
+    let text = document.createElementNS(svgns, "text");
+
+    text.setAttributeNS(null, "x", Xcenter);
+    text.setAttributeNS(null, "y", Ycenter+5); //5 is the offset
+    text.setAttributeNS(null, "font-size", size);
+    text.setAttributeNS(null, "fill", 'black');
+    text.setAttributeNS(null, "text-anchor", 'middle');
+    text.setAttributeNS(null, "alignment-baseline", 'middle');
+    text.setAttributeNS(null, "font-family", 'Arial');
+    text.setAttributeNS(null, "font-weight", 'bold');
+
+    text.textContent = letter;
+    svg.appendChild(text);
 }
-createPentagon(`polygon_0`, 50, 135, 140, 36, `white`)
+
+function isValidWord(word) {
+    // check if word exists in english dictionary
+    const url = `https://api.datamuse.com/words?rel_jja=${word}`;
+    const response = fetch(url);
+
+    return response.then(response => response.json()).then(data => {
+        if (data.length > 0) {
+            return true;
+        }
+        return false;
+    });
+}
+
+
+createPentagon(`polygon_0`, 50, 135, 140, 36, `white`, `A`);
 
 for (var i = 0; i < 5;i++) {
     var curStep = i * (2 * Math.PI / 5) + ((Math.PI / 180.0) * -18);
-    createPentagon(`polygon_${i+1}`, 50, 135+ (90 * Math.cos(curStep)), 140 + (90 * Math.sin(curStep)), 0, `red`);
+    createPentagon(`polygon_${i+1}`, 50, 135+ (90 * Math.cos(curStep)), 140 + (90 * Math.sin(curStep)), 0, `red`, `${i+1}`);
 }
 
 for (var i=0; i < document.getElementsByTagName(`g`).length; i++) {
     let polygon = document.getElementById(`polygon_${i}`);
     polygon.addEventListener('mouseover', function(e) {
-        polygon.firstChild.setAttribute('fill', 'white');
+        e.preventDefault();
+        if (polygon.getAttribute('id') == 'polygon_0') {
+            polygon.firstChild.setAttribute('fill', 'red');
+        } else {
+            polygon.firstChild.setAttribute('fill', 'white');
+        }
     })
     polygon.addEventListener('mouseout', function(e) {
-        polygon.firstChild.setAttribute('fill', 'red');
+        e.preventDefault();
+        if (polygon.getAttribute('id') == 'polygon_0') {
+            polygon.firstChild.setAttribute('fill', 'white');
+        } else {
+            polygon.firstChild.setAttribute('fill', 'red');
+        }
     })
     polygon.addEventListener('click', function(e) {
-        console.log(`you clicked ${i}`);
+        console.log(`you clicked ${polygon.lastChild.textContent}`);
     })
 }
